bfc51a09765c24e55bbb1fefdea74ec85fd6700b
Omkar Parte <rakmoparte@gmail.com>
2026-03-01 20:49:48 +0530

    rodin: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Signed-off-by: Omkar Parte <88646966+RAM-UNLOK@users.noreply.github.com>
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -93,13 +93,17 @@ blob_fixups: blob_fixups_user_type = {
     ('odm/lib64/libmt_mitee.so', 'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee'): blob_fixup()
         .replace_needed('android.hardware.security.keymint-V3-ndk.so', 'android.hardware.security.keymint-V3-ndk-v34.so'),
     'odm/bin/hw/vendor.xiaomi.sensor.citsensorservice.aidl': blob_fixup()
-        .add_needed('libui_shim.so'),
+        .add_needed('libui_shim.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+    'vendor/bin/hw/vendor.xiaomi.hardware.displayfeature_aidl-service': blob_fixup()
+        .add_needed('libutils-v32.so'),
+    'odm/lib64/libframecapturemanager.so': blob_fixup()
+        .add_needed('libutils-v32.so'),
     (
         'vendor/bin/mnld',
         'vendor/lib64/mt6899/libpqconfig.so',
         'vendor/lib64/mt6899/libaalservice.so',
-        'odm/bin/hw/vendor.xiaomi.sensor.citsensorservice.aidl',
-        'odm/lib64/hw/displayfeature.default.so',
         'odm/lib64/libpaperMode.so',
         'odm/lib64/libmiBrightness.so',
         'odm/lib64/libmiSensorCtrl.so',
@@ -153,14 +157,19 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/libcameraopt.so',
      'vendor/lib64/mt6899/libcam.hal3a.so',
      'vendor/lib64/mt6899/libcam.hal3a.ctrl.so',
-     'vendor/lib64/mt6899/libmtkcam_taskmgr.so',
-     'vendor/lib64/hw/hwcomposer.mtk_common.so'): blob_fixup()
+     'vendor/lib64/mt6899/libmtkcam_taskmgr.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
-    ('vendor/lib64/libmicamera_hal_core.so',
-     'vendor/lib64/libmialgoengine.so',
+    'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    ('vendor/lib64/libmialgoengine.so',
      'vendor/lib64/libcom.xiaomi.grallocutils.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so')
         .call(blob_fixup_graphic_buffer_size),
+    'vendor/lib64/libmicamera_hal_core.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .call(blob_fixup_graphic_buffer_size)
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     ('vendor/lib64/mt6899/libneuralnetworks_sl_driver_mtk_prebuilt.so',
      'odm/lib64/libwa_widelens_undistort.so',
      'odm/lib64/libarcsoft_beautyshot.so',
@@ -188,6 +197,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.audio.effect-V2-ndk.so', 'android.hardware.audio.effect-V3-ndk.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v34.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
         .replace_needed('libxlog.so', 'libxlog_stub.so'),
     'vendor/lib64/libdlbdsservice.so': blob_fixup()
         .replace_needed("libstagefright_foundation.so", "libstagefright_foundation-v33.so"),
@@ -196,6 +206,21 @@ blob_fixups: blob_fixups_user_type = {
     "odm/bin/hw/vendor.xiaomi.hw.touchfeature-service": blob_fixup()
         .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so')
         .replace_needed('vendor.xiaomi.hw.touchfeature-V1-ndk.so', 'vendor.xiaomi.hw.touchfeature-V1-ndk-prebuilt.so'),
+    'odm/lib64/hw/displayfeature.default.so': blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    (
+        'vendor/lib64/hw/mt6899/vendor.mediatek.hardware.pq_aidl-impl.so',
+        'vendor/lib64/mt6899/libmmlpqImpl.so',
+        'vendor/lib64/libsilkybrightnesscore.so',
+        'vendor/lib64/libaudiocloudctrl.so',
+        'vendor/lib64/librt_extamp_intf.so',
+        'vendor/lib64/libpqxmlparser.so',
+        'vendor/lib64/libmicamera_aidl_provider.so',
+        'vendor/lib64/libpqxmlflagparser.so',
+        'odm/lib64/libmiXmlParser.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
