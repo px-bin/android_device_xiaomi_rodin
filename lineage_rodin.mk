64abb50dead687ab78bf1692946f01712ecf1bb2
Omkar Parte <rakmoparte@gmail.com>
2026-04-03 21:54:27 +0530

    rodin: Update metadata and fingerprints to OS3.0.10.0.WOJMIXM
    Update the device tree to align with the OS3.0.10.0.WOJMIXM (Android 16)
    firmware release.
    
    - Update Vendor Security Patch level to 2026-02-01.
    - Update Boot Security Patch level to 2026-03-01.
    - Update build descriptions and fingerprints across all region SKUs.
    - Add logo.img to the firmware extraction list (proprietary-firmware.txt).
    - Refresh blob tracking headers in proprietary-files.txt.
    
    Source: rodin_global-ota_full-OS3.0.10.0.WOJMIXM-user-16.0-7ea48324a1
    
    Signed-off-by: Omkar Parte <88646966+RAM-UNLOK@users.noreply.github.com>

--- lineage_rodin.mk
+++ lineage_rodin.mk
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_DEVICE := rodin
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.204.0.VOJMIXM release-keys" \
-    BuildFingerprint=POCO/rodin_global/rodin:15/AP3A.240905.015.A2/OS2.0.204.0.VOJMIXM:user/release-keys \
+    BuildDesc="missi-user 16 BP2A.250605.031.A3 OS3.0.10.0.WOJMIXM release-keys" \
+    BuildFingerprint=POCO/rodin_global/rodin:15/AP3A.240905.015.A2/OS3.0.10.0.WOJMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
