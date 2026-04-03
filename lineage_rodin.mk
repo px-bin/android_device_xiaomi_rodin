65863dc62309dc4e780a2157f0fb1421232fdcc6
pedro <pxd.bin@gmail.com>
2026-04-03 09:16:51 -0300

    enable UDFPS animations

--- lineage_rodin.mk
+++ lineage_rodin.mk
@@ -14,6 +14,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from rodin device
 $(call inherit-product, device/xiaomi/rodin/device.mk)
 
+# FOD
+TARGET_HAS_UDFPS := true
+
 #crDroid flags
 IS_PHONE := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
