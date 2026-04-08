32bdcc49b4a3902e395c84afa9c1fe84e82d84d8
Omkar Parte <rakmoparte@gmail.com>
2026-04-08 05:51:23 +0530

    rodin: Drop Dolby Codecs and associated fixups
    - Comment out Dolby-related Codec2 libraries in proprietary-files.txt:
        * libcodec2_soft_ac4dec.so
        * libcodec2_soft_ddpdec.so
        * libcodec2_store_dolby.so
    - Remove corresponding blob_fixup rules from extract-files.py.
    - This cleans up the media stack by removing proprietary Dolby components 
      not required for basic functionality.
    
    Signed-off-by: Omkar Parte <88646966+RAM-UNLOK@users.noreply.github.com>

--- extract-files.py
+++ extract-files.py
@@ -296,10 +296,7 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libcodec2_hidl@1.0-mtk.so',
         'vendor/lib64/libcodec2_hidl@1.1-mtk.so',
         'vendor/lib64/libcodec2_hidl@1.2-mtk.so',
-        'vendor/lib64/libcodec2_soft_ac4dec-mtk.so',
         'vendor/lib64/libcodec2_soft_common-mtk.so',
-        'vendor/lib64/libcodec2_soft_ddpdec-mtk.so',
-        'vendor/lib64/libcodec2_store_dolby-mtk.so',
     ): blob_fixup()
         .replace_needed('libcodec2.so', 'libcodec2-mtk.so')
         .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-mtk.so'),
@@ -310,10 +307,7 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libcodec2_hidl@1.0-mtk.so',
         'vendor/lib64/libcodec2_hidl@1.1-mtk.so',
         'vendor/lib64/libcodec2_hidl@1.2-mtk.so',
-        'vendor/lib64/libcodec2_soft_ac4dec-mtk.so',
         'vendor/lib64/libcodec2_soft_common-mtk.so',
-        'vendor/lib64/libcodec2_soft_ddpdec-mtk.so',
-        'vendor/lib64/libcodec2_store_dolby-mtk.so',
     ): blob_fixup()
         .replace_needed('libcodec2_hal_common.so', 'libcodec2_hal_common-mtk.so'),
 
@@ -344,11 +338,6 @@ blob_fixups: blob_fixups_user_type = {
     # Codec2
     'vendor/lib64/libcodec2_hidl@1.2-mtk.so': blob_fixup()
         .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-mtk.so'),
-
-    # Codec2 Dolby store
-    'vendor/lib64/libcodec2_store_dolby-mtk.so': blob_fixup()
-        .replace_needed('libcodec2_soft_ac4dec.so', 'libcodec2_soft_ac4dec-mtk.so')
-        .replace_needed('libcodec2_soft_ddpdec.so', 'libcodec2_soft_ddpdec-mtk.so'),
 
 } # fmt: skip
 
